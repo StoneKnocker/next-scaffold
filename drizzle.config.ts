ce73a81fd312fbf36b7c29a0b9184541c6af1b78
xianyang <xianyang.lu@vcg.com>
2024-06-06 18:03:23 +0800

    fix

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,12 +2,26 @@
 
 import { defineConfig } from "drizzle-kit";
 
+// export default defineConfig({
+//   dialect: "postgresql",
+//   schema: "./src/db/schema.ts",
+//   out: "./src/db/migrations",
+//   driver: "pg",
+//   dbCredentials: {
+//     connectionString: process.env.POSTGRES_URL!,
+//   },
+//   verbose: true,
+//   strict: true,
+// });
+
+ 
 export default defineConfig({
   schema: "./src/db/schema.ts",
-  out: "./src/db/migrations",
-  driver: "pg",
+  out: "./drizzle",
+  dialect: 'postgresql',
   dbCredentials: {
-    connectionString: process.env.POSTGRES_URL!,
+    url: process.env.POSTGRES_URL!,
+    // connectionString: process.env.POSTGRES_URL!,
   },
   verbose: true,
   strict: true,
